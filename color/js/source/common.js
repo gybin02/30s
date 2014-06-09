28ef47fba5867d9551c8bd90ecf91acff1908332
oikewll <oikewll@gmail.com>
2014-06-09 23:33:29 +0800

    COLOR- use base64 for instand img query

--- color/js/source/common.js
+++ color/js/source/common.js
@@ -33,16 +33,7 @@ common.playCountNum = function(){
 //预加载游戏文件
 common.cacheIcon = function(){
 	var tpl = '', cache = ['images/icon-over-1.png', 
-						'images/icon-over-2.png',
-						'images/mask/mask-00.png',
-						'images/mask/mask-01.png',
-						'images/mask/mask-02.png',
-						'images/mask/mask-03.png',
-						'images/mask/mask-04.png',
-						'images/mask/mask-05.png',
-						'images/mask/mask-06.png',
-						'images/mask/mask-07.png',
-						'images/mask/mask-08.png'];
+						'images/icon-over-2.png'];
 	for(var i = 0;i < cache.length;i++){
 		tpl += '<img src="'+cache[i]+'" />';
 	}
@@ -184,7 +175,7 @@ common.setTopic = function(){
 
 	//随机遮罩模板
 	var rd9  = Math.random() * 9 >> 0,
-		maskImg = 'images/mask/mask-0'+rd9+'.png';
+		maskImg = maskData[rd9];
 
 	var color = common.color[ram][0],
 		mask   = common.color[ram][1];
@@ -201,7 +192,7 @@ common.setTopic = function(){
 
 	//提出问题
 	$("color-wrap").setAttribute("style","background-color:#"+color);
-	$("color-mask").setAttribute("style","background-image:url("+maskImg+")");
+	$("color-mask").setAttribute("style","background-image:url(data:image/png;base64,"+maskImg+")");
 
 	//正确答案
 	$("result-"+rd4).innerHTML = mask;
